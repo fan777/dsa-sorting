fd53864446e2cb4d14b3a5ad2e64142dc6a851c3
fan777 <fan777@gmail.com>
2021-07-28 22:45:14 -0700

    basic sorts

--- bubble.js
+++ bubble.js
@@ -1,3 +1,14 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let x = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = x
+      }
+    }
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,38 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let retArr = [];
+  let x = 0
+  let y = 0
 
-function mergeSort() {}
+  while (x < arr1.length && y < arr2.length) {
+    if (arr1[x] < arr2[y]) {
+      retArr.push(arr1[x]);
+      x++;
+    } else {
+      retArr.push(arr2[y]);
+      y++;
+    }
+  }
+  while (x < arr1.length) {
+    retArr.push(arr1[x]);
+    x++;
+  }
+  while (y < arr2.length) {
+    retArr.push(arr2[y]);
+    y++;
+  }
+  return retArr;
+}
 
-module.exports = { merge, mergeSort};
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2)
+  // let first = arr.splice(0, mid)
+  // if (first.length > 1) mergeSort(first)
+  // let second = arr.splice(mid)
+  // if (second.length > 1) mergeSort(second)
+  let first = mergeSort(arr.slice(0, mid))
+  let second = mergeSort(arr.slice(mid))
+  return merge(first, second)
+}
+
+module.exports = { merge, mergeSort };
